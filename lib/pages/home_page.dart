579e3a379b39469b7752cd9509e59de48f36f83a
bebyyf <bebifindia@gmail.com>
2025-10-25 23:09:38 +0700

    Langkah 1-7 Praktikum 5

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import '../models/item.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  // Daftar data item
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000),
+    Item(name: 'Coffee', price: 10000),
+    Item(name: 'Rice', price: 12000),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(8),
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/item');
+              },
+              child: Card(
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Container(
+                  margin: const EdgeInsets.all(12),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          item.name,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          'Rp ${item.price}',
+                          textAlign: TextAlign.end,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/item_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ItemPage extends StatelessWidget {
+  const ItemPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Item Page')),
+      body: const Center(
+        child: Text('Ini adalah Item Page.', style: TextStyle(fontSize: 20)),
+      ),
+    );
+  }
+}
